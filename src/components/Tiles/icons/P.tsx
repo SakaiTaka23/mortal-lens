1985cd03546b83bc68c35f493bc422af74bd3377
SakaiTaka23 <tt3.sakai@gmail.com>
2025-04-27 11:09:18 +0900

    generated icons

---
+++ src/components/Tiles/icons/P.tsx
@@ -0,0 +1,5 @@
+import type { SVGProps } from "react";
+const SvgP = (props: SVGProps<SVGSVGElement>) => (
+  <svg xmlns="http://www.w3.org/2000/svg" width={300} height={400} {...props} />
+);
+export default SvgP;
