e1de953f72e834d0004872911bac5d1eab23571b
SakaiTaka23 <tt3.sakai@gmail.com>
2025-05-17 17:01:21 +0900

    deleted to start from scratch

--- src/types/common/PlayerID.ts
+++
@@ -1,9 +0,0 @@
-import { z } from 'zod';
-
-export const PlayerIDSchema = z.union([
-  z.literal(0),
-  z.literal(1),
-  z.literal(2),
-  z.literal(3),
-]);
-export type PlayerID = z.infer<typeof PlayerIDSchema>;
--- src/types/output/DiffLevel.ts
+++
@@ -1,9 +0,0 @@
-import { z } from 'zod';
-
-export const DiffLevelSchema = z.enum([
-  'optimal',
-  'moderate',
-  'significant',
-  'critical',
-]);
-export type DiffLevel = z.infer<typeof DiffLevelSchema>;
