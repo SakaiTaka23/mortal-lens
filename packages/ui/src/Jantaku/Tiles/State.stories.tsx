758a1b049b3c0fff9bd7743c8a34eb0f232915a5
SakaiTaka23 <tt3.sakai@gmail.com>
2025-06-27 21:27:20 +0900

    implemented state to use position

--- packages/ui/src/Jantaku/Tiles/State.stories.tsx
+++ packages/ui/src/Jantaku/Tiles/State.stories.tsx
@@ -1,3 +1,4 @@
+import { HandState } from '@mjai/core';
 import type { Meta, StoryObj } from '@storybook/react';
 
 import { State } from './State';
@@ -33,6 +34,7 @@ export const SimpleHand: Story = {
     ],
     tsumo: null,
     fuuros: [],
+    position: 'self',
   },
 };
 
@@ -49,6 +51,7 @@ export const WithPon: Story = {
         target: 1,
       },
     ],
+    position: 'self',
   },
 };
 
@@ -79,6 +82,7 @@ export const WithMultipleFuuros: Story = {
         consumed: ['P', 'P', 'P'],
       },
     ],
+    position: 'self',
   },
 };
 
@@ -102,5 +106,41 @@ export const ComplexHand: Story = {
         consumed: ['W', 'W'],
       },
     ],
+    position: 'self',
+  },
+};
+
+const defaultArgs = {
+  tehai: ['1m', '2m', '3m', '4m', '5m', '6m', '7m', '8m', '9m', '1p'],
+  tsumo: '1p',
+  fuuros: [
+    {
+      type: 'pon',
+      actor: 0,
+      pai: '5mr',
+      consumed: ['5m', '5m'],
+      target: 1,
+    },
+  ],
+} as HandState;
+
+export const Shimocha: Story = {
+  args: {
+    ...defaultArgs,
+    position: 'shimocha',
+  },
+};
+
+export const Toimen: Story = {
+  args: {
+    ...defaultArgs,
+    position: 'toimen',
+  },
+};
+
+export const Kamicha: Story = {
+  args: {
+    ...defaultArgs,
+    position: 'kamicha',
   },
 };
--- packages/ui/src/Jantaku/Tiles/State.tsx
+++ packages/ui/src/Jantaku/Tiles/State.tsx
@@ -4,10 +4,44 @@ import React from 'react';
 
 import { Fuuros } from './Fuuros';
 import { Tehai } from './Tehai';
+import { Position } from '../types';
 
-export const State: React.FC<HandState> = ({ tehai, tsumo, fuuros }) => (
-  <Stack direction='row' spacing={2} sx={{ alignItems: 'flex-end' }}>
-    <Tehai tehai={tehai} tsumo={tsumo} />
-    <Fuuros fuuros={fuuros} />
-  </Stack>
-);
+export interface Props extends HandState {
+  position: Position;
+}
+
+const POSITION_CONFIG = {
+  self: {
+    direction: 'row' as const,
+    fuuroPosition: 'end' as const,
+  },
+  toimen: {
+    direction: 'row' as const,
+    fuuroPosition: 'start' as const,
+  },
+  kamicha: {
+    direction: 'column' as const,
+    fuuroPosition: 'end' as const,
+  },
+  shimocha: {
+    direction: 'column' as const,
+    fuuroPosition: 'start' as const,
+  },
+};
+
+export const State: React.FC<Props> = ({ tehai, tsumo, fuuros, position }) => {
+  const config = POSITION_CONFIG[position];
+  const fuurosComponent = <Fuuros fuuros={fuuros} position={position} />;
+
+  return (
+    <Stack
+      direction={config.direction}
+      spacing={2}
+      sx={{ alignItems: 'flex-end' }}
+    >
+      {config.fuuroPosition === 'start' && fuurosComponent}
+      <Tehai tehai={tehai} tsumo={tsumo} position={position} />
+      {config.fuuroPosition === 'end' && fuurosComponent}
+    </Stack>
+  );
+};
