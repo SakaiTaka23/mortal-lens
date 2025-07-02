51bb6d6ea04428ecb81e38afa30298ad60533ef5
SakaiTaka23 <tt3.sakai@gmail.com>
2025-07-02 22:15:05 +0900

    created overview detail

---
+++ packages/ui/src/review/OverviewDetail.stories.tsx
@@ -0,0 +1,101 @@
+import { EvaluationDetail } from '@mortal-lens/types';
+import { Meta, StoryObj } from '@storybook/react';
+
+import { OverviewDetail } from '@/review/OverviewDetail';
+
+const meta = {
+  component: OverviewDetail,
+  title: 'OverviewDetail',
+  tags: ['autodocs'],
+} satisfies Meta<typeof OverviewDetail>;
+
+export default meta;
+type Story = StoryObj<typeof OverviewDetail>;
+
+const sampleData: EvaluationDetail[] = [
+  {
+    action: { type: 'ankan', actor: 0, consumed: ['1m', '1m', '1m', '1m'] },
+    QValue: -0.044795454,
+    prob: 0.7110758,
+  },
+  {
+    action: {
+      type: 'chi',
+      actor: 1,
+      target: 3,
+      pai: '4m',
+      consumed: ['2m', '3m'],
+    },
+    QValue: -0.2161197,
+    prob: 0.12819295,
+  },
+  {
+    action: {
+      type: 'pon',
+      actor: 2,
+      target: 1,
+      pai: '5p',
+      consumed: ['5p', '5p'],
+    },
+    QValue: -0.2291053,
+    prob: 0.112581864,
+  },
+  {
+    action: { type: 'dahai', actor: 3, pai: '9s', tsumogiri: true },
+    QValue: -0.31504124,
+    prob: 0.0476709,
+  },
+  {
+    action: {
+      type: 'daiminkan',
+      actor: 0,
+      target: 2,
+      pai: '7m',
+      consumed: ['7m', '7m', '7m'],
+    },
+    QValue: -0.80172265,
+    prob: 0.00036696362,
+  },
+  {
+    action: {
+      type: 'kakan',
+      actor: 1,
+      pai: '3p',
+      consumed: ['3p', '3p', '3p'],
+    },
+    QValue: -1.0375156,
+    prob: 0.00003472058,
+  },
+  {
+    action: { type: 'hora', actor: 2, target: 1 },
+    QValue: -1.0922747,
+    prob: 0.000020080362,
+  },
+  {
+    action: { type: 'none' },
+    QValue: -1.1031547,
+    prob: 0.000018010274,
+  },
+  {
+    action: { type: 'reach', actor: 3 },
+    QValue: -1.1262641,
+    prob: 0.000014294116,
+  },
+  {
+    action: { type: 'ryukyoku', deltas: [25000, 25000, 25000, 25000] },
+    QValue: -1.1306174,
+    prob: 0.0000136852095,
+  },
+];
+
+export const Default: Story = {
+  args: {
+    detail: sampleData.slice(0, 2),
+  },
+};
+
+export const AllDetail: Story = {
+  args: {
+    detail: sampleData,
+  },
+};
---
+++ packages/ui/src/review/OverviewDetail.tsx
@@ -0,0 +1,104 @@
+import { EvaluationDetail, EvaluationResult } from '@mortal-lens/types';
+import {
+  Box,
+  Paper,
+  Stack,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+} from '@mui/material';
+import {
+  createColumnHelper,
+  flexRender,
+  getCoreRowModel,
+  useReactTable,
+} from '@tanstack/react-table';
+import React from 'react';
+
+import { ReviewTile } from '@/review/ReviewTile';
+
+import { ReviewMessage } from './ReviewMessage';
+
+export interface Props {
+  detail: EvaluationDetail[];
+}
+
+const columnHelper = createColumnHelper<EvaluationDetail>();
+
+const columns = [
+  columnHelper.accessor('action', {
+    header: 'Action',
+    cell: ({ row }) => {
+      const action = row.getValue<EvaluationResult>('action');
+      return (
+        <Stack direction='row' spacing={3}>
+          <ReviewMessage result={action} />
+          <ReviewTile result={action} />
+        </Stack>
+      );
+    },
+    footer: (info) => info.column.id,
+  }),
+  columnHelper.accessor('QValue', {
+    header: 'Q Value',
+    cell: (info) => {
+      const value = info.getValue<number>();
+      return value.toFixed(2);
+    },
+    footer: (info) => info.column.id,
+  }),
+  columnHelper.accessor('prob', {
+    header: 'Probability',
+    cell: (info) => {
+      const prob = info.getValue<number>();
+      return (Math.round(prob * 10000) / 100).toFixed(2);
+    },
+    footer: (info) => info.column.id,
+  }),
+];
+
+export const OverviewDetail: React.FC<Props> = ({ detail }) => {
+  const table = useReactTable({
+    data: detail,
+    columns,
+    getCoreRowModel: getCoreRowModel(),
+  });
+  return (
+    <Box sx={{ width: '100%' }}>
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 650 }} aria-label='overview table'>
+          <TableHead>
+            {table.getHeaderGroups().map((headerGroup) => (
+              <TableRow key={headerGroup.id}>
+                {headerGroup.headers.map((header) => (
+                  <TableCell key={header.id} colSpan={header.colSpan}>
+                    {header.isPlaceholder
+                      ? null
+                      : flexRender(
+                          header.column.columnDef.header,
+                          header.getContext(),
+                        )}
+                  </TableCell>
+                ))}
+              </TableRow>
+            ))}
+          </TableHead>
+          <TableBody>
+            {table.getRowModel().rows.map((row) => (
+              <TableRow key={row.id} hover>
+                {row.getVisibleCells().map((cell) => (
+                  <TableCell key={cell.id}>
+                    {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                  </TableCell>
+                ))}
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </Box>
+  );
+};
