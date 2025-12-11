7e578c09e8d2080a7abc81fc967d20523bcee764
SakaiTaka23 <tt3.sakai@gmail.com>
2025-12-11 21:56:23 +0900

    implemented mordal

--- packages/ui/src/index.tsx
+++ packages/ui/src/index.tsx
@@ -1,14 +1,15 @@
 import { Control } from '@/control';
+import { DiffOverviewTable } from '@/diff';
+import { Jantaku } from '@/Jantaku';
+import { KyokuEndScoreModal } from '@/mordal';
 import { Overview } from '@/review/Overview';
 import { OverviewDetail } from '@/review/OverviewDetail';
 import { ReviewWindow } from '@/review/ReviewWindow';
-
-import { DiffOverviewTable } from './diff';
-import { Jantaku } from './Jantaku';
-import theme from './theme';
+import theme from '@/theme';
 
 export { Control };
 export { Jantaku };
 export { Overview, OverviewDetail, ReviewWindow };
+export { KyokuEndScoreModal };
 export { DiffOverviewTable };
 export { theme };
