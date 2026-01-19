022167431e3e93a65e12204f66a23bd2d08153e0
SakaiTaka23 <tt3.sakai@gmail.com>
2026-01-19 22:41:20 +0900

    delete glass theme

--- packages/ui/src/theme.ts
+++ packages/ui/src/theme.ts
@@ -4,7 +4,6 @@ const theme = createTheme({
   palette: {
     background: {
       default: 'rgba(255,255,255,0.15)',
-      paper: 'rgba(255,255,255,0.25)',
     },
     primary: {
       main: '#3b82f6',
@@ -20,18 +19,6 @@ const theme = createTheme({
     },
     info: {
       main: '#34d399',
-    },
-  },
-  components: {
-    MuiPaper: {
-      styleOverrides: {
-        root: {
-          backdropFilter: 'blur(8px)',
-          WebkitBackdropFilter: 'blur(8px)',
-          boxShadow: '0 8px 32px 0 rgba(31, 38, 135, 0.37)',
-          border: '1px solid rgba(255,255,255,0.4)',
-        },
-      },
     },
   },
 });
