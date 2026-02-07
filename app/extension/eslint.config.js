4e101e746dda0322adfc0bc23aaad9085323be63
SakaiTaka23 <tt3.sakai@gmail.com>
2026-02-07 21:22:23 +0900

    optimized for current setting

--- app/extension/eslint.config.js
+++ app/extension/eslint.config.js
@@ -3,6 +3,6 @@ import { ui } from '@mortal-lens/eslint-config/ui';
 export default [
   ...ui,
   {
-    ignores: ['.output/**', '.wxt/**', '**/*.config.*', 'node_modules/**'],
+    ignores: ['dist/**', '.wxt/**', '**/*.config.*', 'node_modules/**'],
   },
 ];
